171bbe4668bca1df00153536267e85369a18334b
dliesenfelt <liesenfelt.code@gmail.com>
2022-03-07 20:41:20 -0800

    added react express view lib

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,15 +1,20 @@
 const express = require('express')
+const bread = require('../models/bread.js')
 const breads = express.Router()
 const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-  res.send(Bread)
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
+// res.send(Bread)
 })
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    const pickedBread = Bread[Number(req.params.arrayIndex)]
     res.send(Bread[req.params.arrayIndex])
 })
 
